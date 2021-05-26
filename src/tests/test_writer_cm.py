acfd96df7166ecf4f04ca601ab24275593fd8310
Derek Wan <d.wan@icloud.com>
2021-05-27 07:07:41 +0800

    Move to poetry setup

--- src/tests/test_writer_cm.py
+++ src/tests/test_writer_cm.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from pathlib import Path
 from stat import S_IRUSR
 from stat import S_IRWXG
@@ -6,7 +8,6 @@ from stat import S_IRWXU
 from stat import S_IWUSR
 from stat import S_IXUSR
 from sys import platform
-from typing import Union
 
 from pytest import mark
 from pytest import param
@@ -15,7 +16,7 @@ from pytest import raises
 from writer_cm import writer_cm
 
 
-@mark.parametrize(  # type: ignore
+@mark.parametrize(
     "is_binary, contents",
     [
         param(False, "contents", id="text"),
@@ -23,23 +24,21 @@ from writer_cm import writer_cm
     ],
 )
 def test_basic_usage(
-    tmp_path: Path,
-    is_binary: bool,
-    contents: Union[str, bytes],
+    tmp_path: Path, is_binary: bool, contents: str | bytes
 ) -> None:
     path = tmp_path.joinpath("file.txt")
-    with writer_cm(path) as temp:
-        with open(temp, mode="wb" if is_binary else "w") as fh1:
-            fh1.write(contents)
+    with writer_cm(path) as temp, open(
+        temp, mode="wb" if is_binary else "w"
+    ) as fh1:
+        fh1.write(contents)
     with open(str(path), mode="rb" if is_binary else "r") as fh2:
         assert fh2.read() == contents
 
 
 def test_dir_perms(tmp_path: Path) -> None:
     path = tmp_path.joinpath("dir1/dir2/dir3/file.txt")
-    with writer_cm(path, dir_perms=S_IRWXU) as temp:
-        with open(temp, mode="w") as fh:
-            fh.write("contents")
+    with writer_cm(path, dir_perms=S_IRWXU) as temp, open(temp, mode="w") as fh:
+        fh.write("contents")
     parts = path.relative_to(tmp_path).parent.parts
     for idx, _ in enumerate(parts):
         path_parent = tmp_path.joinpath(*parts[: (idx + 1)])
@@ -48,25 +47,24 @@ def test_dir_perms(tmp_path: Path) -> None:
 
 
 @mark.skipif(  # type: ignore
-    platform == "win32",
-    reason=r"re.error: incomplete escape \U at position 2",
+    platform == "win32", reason=r"re.error: incomplete escape \U at position 2"
 )
 def test_file_exists_error(tmp_path: Path) -> None:
     path = tmp_path.joinpath("file.txt")
-    with writer_cm(path) as temp1:
-        with open(temp1, mode="w") as fh1:
-            fh1.write("contents")
-    with raises(FileExistsError, match=str(path)):
-        with writer_cm(path) as temp2:
-            with open(temp2, mode="w") as fh2:
-                fh2.write("new contents")
+    with writer_cm(path) as temp1, open(temp1, mode="w") as fh1:
+        fh1.write("contents")
+    with raises(FileExistsError, match=str(path)), writer_cm(
+        path
+    ) as temp2, open(temp2, mode="w") as fh2:
+        fh2.write("new contents")
 
 
 def test_file_perms(tmp_path: Path) -> None:
     path = tmp_path.joinpath("file.txt")
-    with writer_cm(path, file_perms=S_IRUSR) as temp:
-        with open(temp, mode="w") as fh:
-            fh.write("contents")
+    with writer_cm(path, file_perms=S_IRUSR) as temp, open(
+        temp, mode="w"
+    ) as fh:
+        fh.write("contents")
     assert (
         path.stat().st_mode
         & S_IRUSR
@@ -79,11 +77,9 @@ def test_file_perms(tmp_path: Path) -> None:
 
 def test_overwrite(tmp_path: Path) -> None:
     path = tmp_path.joinpath("file.txt")
-    with writer_cm(path) as temp1:
-        with open(temp1, mode="w") as fh1:
-            fh1.write("contents")
-    with writer_cm(path, overwrite=True) as temp2:
-        with open(temp2, mode="w") as fh2:
-            fh2.write("new contents")
+    with writer_cm(path) as temp1, open(temp1, mode="w") as fh1:
+        fh1.write("contents")
+    with writer_cm(path, overwrite=True) as temp2, open(temp2, mode="w") as fh2:
+        fh2.write("new contents")
     with open(str(path)) as fh3:
         assert fh3.read() == "new contents"
