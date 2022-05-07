8cd8a99475d90410f771e679bda5beb734d4e191
Derek Wan <d.wan@icloud.com>
2022-05-08 01:34:25 +0800

    Modernize repo (#23)

--- src/tests/test_writer_cm.py
+++ src/tests/test_writer_cm.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 from pathlib import Path
 from stat import S_IRUSR
 from stat import S_IRWXG
@@ -8,6 +6,7 @@ from stat import S_IRWXU
 from stat import S_IWUSR
 from stat import S_IXUSR
 from sys import platform
+from typing import Union
 
 from pytest import mark
 from pytest import param
@@ -24,13 +23,13 @@ from writer_cm import writer_cm
     ],
 )
 def test_basic_usage(
-    tmp_path: Path, is_binary: bool, contents: str | bytes
+    tmp_path: Path, is_binary: bool, contents: Union[str, bytes]
 ) -> None:
     path = tmp_path.joinpath("file.txt")
     with writer_cm(path) as temp, open(
         temp, mode="wb" if is_binary else "w"
     ) as fh1:
-        fh1.write(contents)
+        _ = fh1.write(contents)
     with open(str(path), mode="rb" if is_binary else "r") as fh2:
         assert fh2.read() == contents
 
@@ -38,7 +37,7 @@ def test_basic_usage(
 def test_dir_perms(tmp_path: Path) -> None:
     path = tmp_path.joinpath("dir1/dir2/dir3/file.txt")
     with writer_cm(path, dir_perms=S_IRWXU) as temp, open(temp, mode="w") as fh:
-        fh.write("contents")
+        _ = fh.write("contents")
     parts = path.relative_to(tmp_path).parent.parts
     for idx, _ in enumerate(parts):
         path_parent = tmp_path.joinpath(*parts[: (idx + 1)])
@@ -46,17 +45,17 @@ def test_dir_perms(tmp_path: Path) -> None:
         assert path_parent.stat().st_mode & S_IRWXU & (~S_IRWXG) & (~S_IRWXO)
 
 
-@mark.skipif(  # type: ignore
+@mark.skipif(
     platform == "win32", reason=r"re.error: incomplete escape \U at position 2"
 )
 def test_file_exists_error(tmp_path: Path) -> None:
     path = tmp_path.joinpath("file.txt")
     with writer_cm(path) as temp1, open(temp1, mode="w") as fh1:
-        fh1.write("contents")
+        _ = fh1.write("contents")
     with raises(FileExistsError, match=str(path)), writer_cm(
         path
     ) as temp2, open(temp2, mode="w") as fh2:
-        fh2.write("new contents")
+        _ = fh2.write("new contents")
 
 
 def test_file_perms(tmp_path: Path) -> None:
@@ -64,7 +63,7 @@ def test_file_perms(tmp_path: Path) -> None:
     with writer_cm(path, file_perms=S_IRUSR) as temp, open(
         temp, mode="w"
     ) as fh:
-        fh.write("contents")
+        _ = fh.write("contents")
     assert (
         path.stat().st_mode
         & S_IRUSR
@@ -78,8 +77,8 @@ def test_file_perms(tmp_path: Path) -> None:
 def test_overwrite(tmp_path: Path) -> None:
     path = tmp_path.joinpath("file.txt")
     with writer_cm(path) as temp1, open(temp1, mode="w") as fh1:
-        fh1.write("contents")
+        _ = fh1.write("contents")
     with writer_cm(path, overwrite=True) as temp2, open(temp2, mode="w") as fh2:
-        fh2.write("new contents")
+        _ = fh2.write("new contents")
     with open(str(path)) as fh3:
         assert fh3.read() == "new contents"
